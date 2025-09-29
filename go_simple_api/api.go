d2b89b4fa8d047e19a3b9aeb15e30be1ec25b8d5
John Mark <johnmarkroco05@gmail.com>
2025-09-29 19:23:38 +0800

    Delete go_simple_api directory

--- go_simple_api/api.go
+++
@@ -1,7 +0,0 @@
-//All Go files in package main are linked so we do not need to include the other Go file
-package main
-
-func main(){
-	//We call this function from api_controller.go
-	handleRequest()
-}
--- go_simple_api/go.mod
+++
@@ -1,5 +0,0 @@
-module go_simple_api
-
-go 1.25.0
-
-require github.com/gorilla/mux v1.8.1 // indirect
